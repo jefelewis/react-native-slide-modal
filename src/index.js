ad4533a22aaa17b4b7cb9b1f05c27b96afbe56cb
Jeff Lewis <jeffylew77@gmail.com>
2021-05-18 00:13:32 -0700

    [Dev] Adding index file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Imports: Components
+import SlideModal from './components/SlideModal';
+
+// Imports: TypeScript Types
+// import { ContactItem } from './types/types';
+
+// Exports
+export {
+  // Components
+  SlideModal,
+  // TypeScript Types
+  ContactItem,
+};
