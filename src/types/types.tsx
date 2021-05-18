94a490b5deed93098541de2b2f769c27cf31901a
Jeff Lewis <jeffylew77@gmail.com>
2021-05-18 02:14:21 -0700

    [Dev] Adding types

---
+++ src/types/types.tsx
@@ -0,0 +1,86 @@
+// STYLES
+// TypeScript Type: Font Weight
+type FontWeight = 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900' | undefined;
+
+// TypeScript Type: Text Transform
+type TextTransform = 'uppercase' | 'lowercase' | 'capitalize';
+
+// TypeScript Type: Container Style
+export interface ContainerStyle {
+  containerLight: {
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+  containerDark: {
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+};
+
+// TypeScript Type: Modal Header Container Style
+export interface ModalHeaderContainerStyle {
+  modalHeaderContainerLight: {
+    height?: number,
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+  modalHeaderContainerDark: {
+    height?: number,
+    backgroundColor?: string,
+    borderColor?: string,
+    borderBottomWidth?: number,
+  },
+};
+
+// TypeScript Type: Cancel Text Style
+export interface CancelTextStyle {
+  cancelTextLight: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+  cancelTextDark: {
+    fontFamily?: string,
+    fontSize?: number,
+    fontWeight?: FontWeight,
+    textTransform?: TextTransform,
+    color?: string,
+  },
+};
+
+// TypeScript Type: Done Text Style
+export interface DoneTextStyle {
+  doneTextLight: {
+    color?: string,
+  },
+  doneTextDark: {
+    color?: string,
+  },
+};
+
+// TypeScript Type: Modal Content Container Style
+export interface ModalContentContainerStyle {
+  modalContentContainerLight: {
+    height?: number,
+    backgroundColor?: string,
+  },
+  modalContentContainerDark: {
+    height?: number,
+    backgroundColor?: string,
+  },
+};
+
+// TypeScript Type: Picker Item Text Style
+export interface PickerItemTextStyle {
+    pickerItemTextLight: {
+      color?: string,
+    },
+    pickerItemTextDark: {
+      color?: string,
+    },
+  };
