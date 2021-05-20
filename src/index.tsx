cae2ce0064f2b76fc37a2b33bf3309cc90a83079
Jeff Lewis <jeffylew77@gmail.com>
2021-05-20 12:47:46 -0700

    [Fix] Linting

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@
 import SlideModal from './components/SlideModal';
 
 // Imports: TypeScript Types
-// import { ContactItem } from './types/types';
 
 // Exports
 export {
   // Components
   SlideModal,
   // TypeScript Types
-  // ContactItem,
 };
